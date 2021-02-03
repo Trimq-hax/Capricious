5a03d0ca74352483cb927ad9265618ee669797e5
Trimaqs | Child Delinquents <78451730+Trimaq@users.noreply.github.com>
2021-02-03 08:22:46 -0800

    Update Good-Morning.py

--- CODE/Good-Morning.py
+++ CODE/Good-Morning.py
@@ -2,6 +2,6 @@ print("Hello! how are you doing this morning")
 
 Answer = input()
 
-if Answer == "good":
+if Answer == ("good","great"):
 	print("Good to know human overlord!")
-else:print("I guess today is the day to conquer you then!")
+else: print("I guess today is the day to conquer you then!")
